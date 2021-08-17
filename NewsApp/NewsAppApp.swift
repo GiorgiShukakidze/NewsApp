b1b00d4393f4cf9ad403bcc7e10c753410cb64bd
Giorgi Shukakidze <gshukakidze@gmail.com>
2021-08-17 15:29:10 +0200

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Giorgi Shukakidze on 17.08.21.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
